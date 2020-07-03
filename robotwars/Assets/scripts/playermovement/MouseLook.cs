6783b2531766e0f78c81607506905d3d9874b93c
rm20killer <43253883+rm20killer@users.noreply.github.com>
2020-07-03 15:19:26 +0100

    Gun fire script

--- robotwars/Assets/scripts/playermovement/MouseLook.cs
+++ robotwars/Assets/scripts/playermovement/MouseLook.cs
@@ -7,6 +7,7 @@ public class MouseLook : MonoBehaviour
 {
     public float mouseSpeed = 100f;
     public Transform playerBody;
+    //public Transform Gun;
     float xRotation = 0f;
     void Start()
 
@@ -19,7 +20,7 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -20f, 40f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
---
+++ robotwars/Assets/scripts/weapon scripts/example shot/gunShot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class gunShot : MonoBehaviour
+{
+    public float Range = 100f;
+
+    public GameObject impactEffect;
+    private RaycastHit hit;
+    private Ray ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ViewportPointToRay(new Vector3(.5f, .5f, 0));
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (Physics.Raycast(ray, out hit, Range))
+            {
+                GameObject impactEffectGO = Instantiate(impactEffect, hit.point, Quaternion.identity) as GameObject;
+                Destroy(impactEffectGO,5);
+
+            }
+        }
+    }
+}
